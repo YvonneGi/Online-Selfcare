46e378572bdabea27360b190f0085fd13dd2e61d
juru-juliette <kangabejuliette@gmail.com>
2019-08-08 12:33:36 +0200

    adding form data

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+$firstName =filter_input(INPUT_POST,"Firstname");
+$lastName = filter_input(INPUT_POST,"Lastname");
+$lastName = filter_input(INPUT_POST,"Username");
+$lastName = filter_input(INPUT_POST,"Email");
+$lastName = filter_input(INPUT_POST,"Password");
+$lastName = filter_input(INPUT_POST,"Gender");
+$lastName = filter_input(INPUT_POST,"Age");
+?>
