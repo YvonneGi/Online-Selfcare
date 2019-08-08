38ef084ed323003788918ab0f051752b962b5c90
juru-juliette <kangabejuliette@gmail.com>
2019-08-08 15:14:29 +0200

    adding php codes

--- connect.php
+++ connect.php
@@ -1,9 +1,48 @@
 <?php
 $firstName =filter_input(INPUT_POST,"Firstname");
 $lastName = filter_input(INPUT_POST,"Lastname");
-$lastName = filter_input(INPUT_POST,"Username");
-$lastName = filter_input(INPUT_POST,"Email");
-$lastName = filter_input(INPUT_POST,"Password");
-$lastName = filter_input(INPUT_POST,"Gender");
-$lastName = filter_input(INPUT_POST,"Age");
+$email = filter_input(INPUT_POST,"Email");
+$username = filter_input(INPUT_POST,"Username");
+$password = filter_input(INPUT_POST,"Password");
+$gender = filter_input(INPUT_POST,"Gender");
+if(!empty($firstName)){
+    if(!empty($lastName)){
+        if(!empty($email)){
+            if(!empty($username)){
+                if(!empty($password)){
+                    if(!empt($gender)){
+                         
+                    }
+                }
+            }
+        }
+    }
+
+
+$host = "localhost";
+$dbFirstname = "root";
+$dbLastName = "root";
+$dbEmail = "root";
+$dbUsername = "root";
+$dbPassword = " ";
+$dbGender = "root";
+$dbname ="online-selfcare";
+
+//create connection
+$conn = new mysql($host , $dbFirstname,$dbLastName,$dbEmail,$dbUsername,$dbPassword,$dbGender);
+if(mysql_connect_error()){
+    die("connect error('.mysql_connect_error().')".mysql_connect_error());
+}
+else{
+    $sql ="Insert INTO user-table (firstName,lastName,email,username,password,gender) values ('$firstName,$lastName,$email,$username,$password,$gender')";
+    if ($conn->query($sql)){
+        echo "New record is inserted sucessfully";
+    }
+    else{
+        echo "connection error";
+    }
+    $conn->close();
+}
+}
+
 ?>
